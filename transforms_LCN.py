c8185f5bfa80762c8e97cf59b678bf1cfdf38e07
mnaoya <m.naoya.2016@gmail.comC>
2018-03-06 14:56:04 +0900

    rename of class and slight modification of algorithm

--- transforms_LCN.py
+++ transforms_LCN.py
@@ -7,13 +7,14 @@ from scipy.ndimage.filters import convolve
 
 from . import functional as F
 
-__all__ = ["LocalContrastNormalization"]
+__all__ = ["LCN_Pinto"]
 
-class LocalContrastNormalization(object):
+class LCN_Pinto(object):
     """
     Conduct a local contrast normalization algorithm by
-    Pinto, N., Cox, D. D., and DiCarlo, J. J. (2008). Why is real-world visual object recognition hard?
-     PLoS Comput Biol , 4 . 456 (they called this "Local input divisive normalization")
+    Jarrett, Kevin & Kavukcuoglu, Koray & Ranzato, Marc'Aurelio & Lecun, Yann. (2009).
+     What is the Best Multi-Stage Architecture for Object Recognition?.
+     In Proc Intl Conf on Comput Vis. 12. 10.1109/ICCV.2009.5459469.
 
     the kernel size is controllable by argument kernel_size.
     """
@@ -39,29 +40,30 @@ class LocalContrastNormalization(object):
             Tensor: Normalized Tensor image, in size (C, H, W).
         """
         C, H, W = tensor.size()
-        kernel = np.ones((self.kernel_size, self.kernel_size))
+        kernel = np.ones((self.kernel_size, self.kernel_size)) / (self.kernel_size**2)
 
         arr = np.array(tensor)
-        local_sum_arr = np.array([convolve(arr[c], kernel, mode=self.mode, cval=self.cval)
+        local_avg_arr = np.array([convolve(arr[c], kernel, mode=self.mode, cval=self.cval)
                                   for c in range(C)]) # An array that has shape(C, H, W)
-                                                      # Each element [c, h, w] is the summation of the values
+                                                      # Each element [c, h, w] is the average of the values
                                                       # in the window that has arr[c,h,w] at the center.
-        local_avg_arr = local_sum_arr / (self.kernel_size**2) # The tensor of local averages.
 
         arr_square = np.square(arr)
-        local_sum_arr_square = np.array([convolve(arr_square[c], kernel, mode=self.mode, cval=self.cval)
+        local_avg_arr_square = np.array([convolve(arr_square[c], kernel, mode=self.mode, cval=self.cval)
                                   for c in range(C)]) # An array that has shape(C, H, W)
-                                                      # Each element [c, h, w] is the summation of the values
+                                                      # Each element [c, h, w] is the average of the values
                                                       # in the window that has arr_square[c,h,w] at the center.
+        local_sum_arr_square = local_avg_arr_square * (self.kernel_size**2)
         local_norm_arr = np.sqrt(local_sum_arr_square) # The tensor of local Euclidean norms.
 
 
         local_avg_divided_by_norm = local_avg_arr / local_norm_arr
 
-        result_arr = np.minimum(local_avg_rr, local_avg_divided_by_norm)
+        result_arr = np.minimum(local_avg_arr, local_avg_divided_by_norm)
         return torch.Tensor(result_arr)
 
 
 
     def __repr__(self):
         return self.__class__.__name__ + '(kernel_size={0})'.format(self.kernel_size)
+
